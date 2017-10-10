b221e559d8a9249f63bfa6d6bc043b12079b10d9
JEmilioC <28cllje@gmail.com>
2017-10-10 12:34:01 -0500

    C2-NowWithWatson

--- index.js
+++ index.js
@@ -2,19 +2,51 @@ var app= require('express')();
 var http= require('http').Server(app);
 var io= require('socket.io')(http);
 
+var ConversationV1 = require('watson-developer-cloud/conversation/v1');
+
+var conversation = new ConversationV1({
+  username: 'b56f3160-774b-4c90-9341-dde436c46dc3',
+  password: 'wmTZU13BtHoh',
+  path: { workspace_id: 'be6008e1-1343-4962-b174-79ff8726c1ca' },
+  version_date: '2017-10-10'
+});
+
+
 app.get('/', function(req,res){
 	res.sendFile(__dirname+'/index.html');
-
 })
 
 io.emit('some event', { for: 'everyone' });
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
+    io.emit('chat message', "You: "+msg);
 
+
+conversation.message({input: {text: msg}}, processResponse);
+
+function processResponse(err, response) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  if (response.intents.length > 0) {
+    console.log('Detected intent: #' + response.intents[0].intent);
+  }
+  if (response.intents[0].intent!="Meme") {
+  if (response.output.text.length != 0) {
+      console.log(response.output.text[0]);
+	  io.emit('chat message', "Watson: " +response.output.text[0]);
+  }
+}else {
+	var texto="<img src='/images/memes.jpg' alt='Imagen' />";
+	console.log(response.output.text[0]);
+	io.emit('chat message', "Watson: Este es un momaso");
+}
+}
+
+	});
+});
 http.listen(3000,function(){
 	console.log('listening on port 3000')
 });
